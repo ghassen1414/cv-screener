4ddf009824cb71d05c8c9b0503778fc1ea66cba9
ghassen1414 <Ghassen.Zarrouk@Student.HTW-Berlin.de>
2026-04-19 20:44:27 +0200

    test CORS allow origin for vercel deployment

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://cv-screener-eosin.vercel.app/",  
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
